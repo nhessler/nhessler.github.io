291d393d9488d7c5f444e7628aba42d87f0a8ca1
Nathan Hessler <xmilestegx@gmail.com>
2019-12-14 19:22:16 -0600

    Updates

---
+++ component---src-pages-index-js-e8d711cdba36aa6d288b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{RXBc:function(n,e,t){"use strict";t.r(e);var a=t("q1tI"),c=t.n(a);e.default=function(){return c.a.createElement("h1",null,"Hello Gatsby!")}}}]);
+//# sourceMappingURL=component---src-pages-index-js-e8d711cdba36aa6d288b.js.map
